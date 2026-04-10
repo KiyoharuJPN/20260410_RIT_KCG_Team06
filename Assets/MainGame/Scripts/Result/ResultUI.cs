e39d94aafd03813da3a43e4715c6bafec2648bd0
清春 穆 <1905088203@qq.com>
2026-04-11 03:03:10 +0900

    ResultScene

--- Assets/MainGame/Scripts/Result/ResultUI.cs
+++ Assets/MainGame/Scripts/Result/ResultUI.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ResultUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    TMP_Text scoreText;
+    [SerializeField]
+    TMP_Text killText;
+    [SerializeField]
+    TMP_Text coinText;
+    [SerializeField]
+    TMP_Text nextStage;
+
+
+    private void Awake()
     {
-        
+        // ResultManagerからスコア、キル数、コイン数を取得してUIに反映
+        scoreText.text = "50";// ResultManager.score.ToString();
+        killText.text = "Kills :                            5";// + ResultManager.killCount.ToString();
+        coinText.text = "Coins :                            5";// + ResultManager.coinCount.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            // 次のステージへ遷移
+            SceneManager.LoadScene("TitleScene");
+        }
     }
 }
