6ad06c6ebb2d3cdf954a298f710c2761f3116259
seisei0809 <sima.sei.0809@gmail.com>
2026-04-11 23:31:35 +0900

    Add SceneChanger.Edit TitleSceneManager(audio loop,scene change).

--- Assets/MainGame/Scripts/Scene/Title/TitleSceneManager.cs
+++ Assets/MainGame/Scripts/Scene/Title/TitleSceneManager.cs
@@ -1,10 +1,24 @@
-﻿using UnityEngine;
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TitleSceneManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        AudioManager.Instance.PlayBGM(AudioName.BGMName.TITLE_BGM_NAME);
+        AudioManager.Instance.PlayLoopBGM(AudioName.BGMName.TITLE_BGM_NAME);
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.enterKey.wasPressedThisFrame)
+        {
+            SceneChangeManager.Instance
+                .SetFadeDuration(0.4f, 0.3f)
+                .SetFadeEase(Ease.OutCubic, Ease.InCubic)
+                .SetFadeColor(Color.black)
+                .ChangeScene("GameScene");
+        }
     }
 }
