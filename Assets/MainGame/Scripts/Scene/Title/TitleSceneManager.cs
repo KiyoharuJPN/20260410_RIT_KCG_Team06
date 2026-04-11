04c122d405c5034a6dfcacf2cffe7933d9686745
seisei0809 <sima.sei.0809@gmail.com>
2026-04-11 19:24:16 +0900

    Edit Title'sbgm loop.

--- Assets/MainGame/Scripts/Scene/Title/TitleSceneManager.cs
+++ Assets/MainGame/Scripts/Scene/Title/TitleSceneManager.cs
@@ -5,6 +5,6 @@ public class TitleSceneManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        AudioManager.Instance.PlayBGM(AudioName.BGMName.TITLE_BGM_NAME);
+        AudioManager.Instance.PlayLoopBGM(AudioName.BGMName.TITLE_BGM_NAME);
     }
 }
