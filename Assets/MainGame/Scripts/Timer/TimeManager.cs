987b98e001ed14b9e84049362797f5cdea174122
seisei0809 <sima.sei.0809@gmail.com>
2026-04-12 18:35:32 +0900

    Add TimerSystem.

---
+++ Assets/MainGame/Scripts/Timer/TimeManager.cs
@@ -0,0 +1,31 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class TimeManager : Singleton<TimeManager>
+{
+    /// <summary>
+    /// 時間制限
+    /// </summary>
+    [SerializeField]
+    private float timeLimit = 120;
+    public float TimeLimit => timeLimit;
+
+    private void Update()
+    {
+        timeLimit -= Time.deltaTime;
+        if (timeLimit <= 0)
+        {
+            timeLimit = 0;
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+        SceneChangeManager.Instance
+            .SetFadeDuration(0.4f, 0.3f)
+            .SetFadeEase(Ease.OutCubic, Ease.InCubic)
+            .SetFadeColor(Color.black)
+            .ChangeScene("ResultScene");
+    }
+}
---
+++ Assets/MainGame/Scripts/Timer/TimerUI.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+    /// <summary>
+    /// 表示用テキスト
+    /// </summary>
+    private TextMeshProUGUI text;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+
+        text.text = "00:00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = $"{(int)(TimeManager.Instance.TimeLimit / 60):00}:{(int)(TimeManager.Instance.TimeLimit % 60):00}";
+    }
+}
